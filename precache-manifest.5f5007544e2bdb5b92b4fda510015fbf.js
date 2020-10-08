26d13786bd3bd306a74fc7a8bd86e2170167cdc5
cessy <cessycode@gmail.com>
2020-10-08 06:08:29 -0400

    Initial Project Upload

---
+++ precache-manifest.5f5007544e2bdb5b92b4fda510015fbf.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d47a87ab80417d055d8017e7b8a029",
+    "url": "/Portfolio-CeciliaSy/index.html"
+  },
+  {
+    "revision": "39e15b6568f258dbb890",
+    "url": "/Portfolio-CeciliaSy/static/css/2.1777086c.chunk.css"
+  },
+  {
+    "revision": "2ad52b72dead22d4fd44",
+    "url": "/Portfolio-CeciliaSy/static/css/main.7c632267.chunk.css"
+  },
+  {
+    "revision": "39e15b6568f258dbb890",
+    "url": "/Portfolio-CeciliaSy/static/js/2.10b21f0b.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/Portfolio-CeciliaSy/static/js/2.10b21f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad52b72dead22d4fd44",
+    "url": "/Portfolio-CeciliaSy/static/js/main.4122ef62.chunk.js"
+  },
+  {
+    "revision": "8c75ca531f82c20eec67",
+    "url": "/Portfolio-CeciliaSy/static/js/runtime-main.f3c532ed.js"
+  },
+  {
+    "revision": "55c8a4c6989c0c3c6a54cd893f8d9be0",
+    "url": "/Portfolio-CeciliaSy/static/media/ces2.55c8a4c6.png"
+  },
+  {
+    "revision": "32b2268cd58b17195cb711050ac642ee",
+    "url": "/Portfolio-CeciliaSy/static/media/cessyMusic.32b2268c.jpg"
+  },
+  {
+    "revision": "2bdbdb19d51f55951b48c4ded2e0d955",
+    "url": "/Portfolio-CeciliaSy/static/media/cessyMusic1.2bdbdb19.png"
+  },
+  {
+    "revision": "223c997c33da9c1b10587357cc3311f8",
+    "url": "/Portfolio-CeciliaSy/static/media/cessyMusic2.223c997c.png"
+  },
+  {
+    "revision": "955bd68f5ed906f1cc0202c415c5242e",
+    "url": "/Portfolio-CeciliaSy/static/media/face1.955bd68f.png"
+  },
+  {
+    "revision": "8afc3d9a96958e2626b54a4d8a927287",
+    "url": "/Portfolio-CeciliaSy/static/media/face2.8afc3d9a.png"
+  },
+  {
+    "revision": "9853afe95c808d82338dbae01f3de7a3",
+    "url": "/Portfolio-CeciliaSy/static/media/face3.9853afe9.png"
+  },
+  {
+    "revision": "b71bc541b323dcd8f0a887920f9670f9",
+    "url": "/Portfolio-CeciliaSy/static/media/github.b71bc541.png"
+  },
+  {
+    "revision": "51a184173ad9600e6c6cf61a40785f0d",
+    "url": "/Portfolio-CeciliaSy/static/media/healthyEats.51a18417.png"
+  },
+  {
+    "revision": "254729edee38495c299c92bc4d9bc08c",
+    "url": "/Portfolio-CeciliaSy/static/media/healthyEats2.254729ed.png"
+  },
+  {
+    "revision": "757b930aed81b444238603979fb784a9",
+    "url": "/Portfolio-CeciliaSy/static/media/healthyEats3.757b930a.png"
+  },
+  {
+    "revision": "9abb9d626c72ffd665cd9142fab453ff",
+    "url": "/Portfolio-CeciliaSy/static/media/learn-code.9abb9d62.jpg"
+  },
+  {
+    "revision": "c7be3714aec8dc0c067f725016a2a115",
+    "url": "/Portfolio-CeciliaSy/static/media/photo.c7be3714.png"
+  },
+  {
+    "revision": "a8bb2bf17aa49bbff0d36ddb907fce07",
+    "url": "/Portfolio-CeciliaSy/static/media/photo1.a8bb2bf1.png"
+  },
+  {
+    "revision": "b034ab274c7806470ed8a70ad2ceccdb",
+    "url": "/Portfolio-CeciliaSy/static/media/photo2.b034ab27.png"
+  },
+  {
+    "revision": "b932d480bf0e4f0b89bf980d2ec6a1b1",
+    "url": "/Portfolio-CeciliaSy/static/media/planning.b932d480.jpg"
+  },
+  {
+    "revision": "3ad16fb91b5f32941981b3825f12cea2",
+    "url": "/Portfolio-CeciliaSy/static/media/recipe1.3ad16fb9.png"
+  },
+  {
+    "revision": "60dc81c4ddd86c591811a86989776b8f",
+    "url": "/Portfolio-CeciliaSy/static/media/recipe2.60dc81c4.png"
+  },
+  {
+    "revision": "e87595b7fbd612f9d40bda3c801c3ac6",
+    "url": "/Portfolio-CeciliaSy/static/media/recipe3.e87595b7.png"
+  },
+  {
+    "revision": "9d39a5e7532408d409bc1e3d2b832c1a",
+    "url": "/Portfolio-CeciliaSy/static/media/restApi.9d39a5e7.jpg"
+  },
+  {
+    "revision": "9b2945e704ca5f21b767678cea551df4",
+    "url": "/Portfolio-CeciliaSy/static/media/robot1.9b2945e7.png"
+  },
+  {
+    "revision": "c344b3e530574c963882951ddc3d9f02",
+    "url": "/Portfolio-CeciliaSy/static/media/robot2.c344b3e5.png"
+  },
+  {
+    "revision": "0f07ef36a6ef018da4ddeac7fb876194",
+    "url": "/Portfolio-CeciliaSy/static/media/robot4.0f07ef36.png"
+  }
+]);
